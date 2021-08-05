a368b089f419027f1332b035de07ba56de92c53d
lakshna ranasinghe <38062290+lakshanranasingha@users.noreply.github.com>
2021-08-05 23:54:51 +0530

    Material menu added
    Material menu added

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class menu : MonoBehaviour
+{
+    [Header("space between menu items")]
+    [SerializeField] Vector2 spacing;
+
+    Button mainButton;
+    menuItem[] menuItems;
+    bool isExpanded = false;
+
+    Vector2 mainButtonPosition;
+    int itemsCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemsCount = transform.childCount - 1;
+        menuItems = new menuItem[itemsCount];
+        for (int i = 0; i < itemsCount; i++)
+        {
+            menuItems[i] = transform.GetChild(i + 1).GetComponent<menuItem>();
+        }
+
+        mainButton = transform.GetChild(0).GetComponent<Button>();
+        mainButton.onClick.AddListener(ToggleMenu);
+        mainButton.transform.SetAsLastSibling();
+
+        mainButtonPosition = mainButton.transform.position;
+
+        //Reset all menu items position to mainButtonPosition
+
+        ResetPositions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResetPositions ()
+    {
+        for (int i = 0; i < itemsCount; i++)
+        {
+            menuItems[i].trans.position = mainButtonPosition;
+        }
+    }
+
+    void ToggleMenu()
+    {
+        isExpanded = !isExpanded;
+
+        if (isExpanded)
+        {
+            //menu opend
+            for (int i = 0; i < itemsCount; i++)
+            {
+                menuItems[i].trans.position = mainButtonPosition + spacing * (i + 1);
+            }
+        }
+        else
+        {
+            //menu closed
+            for (int i = 0; i < itemsCount; i++)
+            {
+                menuItems[i].trans.position = mainButtonPosition;
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        mainButton.onClick.AddListener(ToggleMenu);
+    }
+}
---
+++ Assets/Scripts/menuItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuItem : MonoBehaviour
+{
+  [HideInInspector] public Image img;
+    [HideInInspector] public Transform trans;
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+        trans = transform;
+    }
+}
