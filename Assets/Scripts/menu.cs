26a729279283a153743caa07e7e87509ac3fb94c
lakshna ranasinghe <38062290+lakshanranasingha@users.noreply.github.com>
2021-08-10 00:48:07 +0530

    Finished the material selection
    Finished the material selection

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+
 public class menu : MonoBehaviour
 {
     [Header("space between menu items")]
@@ -13,9 +14,24 @@ public class menu : MonoBehaviour
     Vector2 mainButtonPosition;
     int itemsCount;
 
+    public GameObject chair;
+    [SerializeField]
+    private Material[] materials;
+
+    Renderer render;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //     Renderer   materials
+
+        render = chair.GetComponent<Renderer>();
+        render.enabled = true;
+        render.sharedMaterial = materials[0];
+
+
+
         itemsCount = transform.childCount - 1;
         menuItems = new menuItem[itemsCount];
         for (int i = 0; i < itemsCount; i++)
@@ -45,6 +61,7 @@ public class menu : MonoBehaviour
         for (int i = 0; i < itemsCount; i++)
         {
             menuItems[i].trans.position = mainButtonPosition;
+            menuItems[i].trans.localScale = new Vector3(0, 0, 0);
         }
     }
 
@@ -58,6 +75,7 @@ public class menu : MonoBehaviour
             for (int i = 0; i < itemsCount; i++)
             {
                 menuItems[i].trans.position = mainButtonPosition + spacing * (i + 1);
+                menuItems[i].trans.localScale = new Vector3(1,1,1);
             }
         }
         else
@@ -66,7 +84,40 @@ public class menu : MonoBehaviour
             for (int i = 0; i < itemsCount; i++)
             {
                 menuItems[i].trans.position = mainButtonPosition;
+                menuItems[i].trans.localScale = new Vector3(0, 0, 0);
             }
+        }
+    }
+
+     public void OnItemClick (int index)
+    {
+       switch (index)
+        {
+            case 0: // 1st button
+                Debug.Log("0");
+                render.sharedMaterial = materials[0];
+                ToggleMenu();
+                break;
+            case 1: // 2nd button
+                Debug.Log("1");
+                render.sharedMaterial = materials[1];
+                ToggleMenu();
+                break;
+            case 2: // 3rd button
+                Debug.Log("2");
+                ToggleMenu();
+                render.sharedMaterial = materials[2];
+                break;
+            case 3: // 4th button
+                Debug.Log("3");
+                ToggleMenu();
+                render.sharedMaterial = materials[3];
+                break;
+            case 4: // 5th button
+                Debug.Log("4");
+                ToggleMenu();
+                render.sharedMaterial = materials[4];
+                break;
         }
     }
 
--- Assets/Scripts/menuItem.cs
+++ Assets/Scripts/menuItem.cs
@@ -8,9 +8,34 @@ public class menuItem : MonoBehaviour
   [HideInInspector] public Image img;
     [HideInInspector] public Transform trans;
 
+    menu menuCS;
+    Button button;
+    int index;
+
+   
+
     void Awake()
     {
+        
+
         img = GetComponent<Image>();
         trans = transform;
+
+        menuCS = trans.parent.GetComponent<menu>();
+        index = trans.GetSiblingIndex() - 1;
+
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnItemClick);
     }
+
+    void OnItemClick()
+    {
+        menuCS.OnItemClick(index);
+    }
+
+    void OnDestroy()
+    {
+        button.onClick.RemoveListener(OnItemClick);
+    }
+
 }
